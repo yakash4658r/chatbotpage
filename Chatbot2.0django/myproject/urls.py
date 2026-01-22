e612b8e512b78ef3fe8fa5b9afc24fc9cb1265c0
Yakash <YV5438@SRMIST.EDU.IN>
2026-01-22 23:05:59 +0530

    Add login and logout paths to urlpatterns

--- Chatbot2.0django/myproject/urls.py
+++ Chatbot2.0django/myproject/urls.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views # Itha marakaama import pannunga
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    
+    # Intha rendu line thaan mukkiyam - 'name' correct-ah irukanum
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
+    
     path('', include('chatbot.urls')),
 ]
