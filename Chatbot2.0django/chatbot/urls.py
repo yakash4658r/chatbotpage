fbcf3915e91d992de13dea4617dec9fc3936bdc4
Yakash <YV5438@SRMIST.EDU.IN>
2026-01-22 23:04:28 +0530

    Update urls.py

--- Chatbot2.0django/chatbot/urls.py
+++ Chatbot2.0django/chatbot/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('pricing/', views.chatbot_pricing, name='pricing'),
     path('create-order/', views.create_order, name='create_order'),
     path('payment-status/', views.payment_status, name='payment_status'),
 ]
